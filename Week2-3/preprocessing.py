a71669f6cbb10a36bdd7fb20962c0130424f5d48
stefanhaannl <31733054+stefanhaannl@users.noreply.github.com>
2017-09-18 13:08:22 +0200

    correct df
    The end Dataframe is now in the correct shape and all values are unpacked.

--- Week2-3/preprocessing.py
+++ Week2-3/preprocessing.py
@@ -19,18 +19,12 @@ def word_in_text(word, text):
     return False
     
 def get_location(boundingBox):
-    # boundingBox = tweet['place']['bounding_box']['coordinates']
-    #loc_dict = []
-    
     i = 0
     for i in range(i,len(boundingBox)):
         boxCoords = boundingBox[i]
         longitude = (boxCoords[0][0]+boxCoords[1][0]+boxCoords[2][0]+boxCoords[3][0])/4
         latitude = (boxCoords[0][1]+boxCoords[1][1]+boxCoords[2][1]+boxCoords[3][1])/4
-    
-    #loc_dict = {'longitude': longitude, 'latitude': latitude}
-    #return loc_dict
-    return longitude, latitude
+    return (longitude, latitude)
     
 def get_adjusted_datetime(text):
     pass 
@@ -46,18 +40,14 @@ def extract_link(text):
 def get_textdict(tweet):
     # divide words
     textlist = str(tweet).split()
-    
-    # init    text_dict = {}
     hashtaglist=[]
     mentionlist = []
     linklist = []
     wordlist = []
     
     # loop through all words
-    i=0
-    for i in range(i, len(textlist)):
-        word = textlist[i]
-        
+
+    for word in textlist:
         # separate charachters
         charachters = list(word)
         
@@ -74,36 +64,22 @@ def get_textdict(tweet):
         else:
             lowerword = word.lower()
             wordlist.append(lowerword)
-       
-    # make text_dict
-    text_dict = {'hashtags':hashtaglist,
-                'mentions':mentionlist,
-                'links':linklist,
-                'text':' '.join(wordlist).lower(),
-                'wordlist': wordlist,
-                }
                 
-    return text_dict
+    return (hashtaglist,mentionlist,linklist,' '.join(wordlist).lower(),wordlist)
     
 def preprocess_dataframe(df):
     # get text_dict
-    df['textinfo'] = df['text'].apply(get_textdict)
+    df['hashtags'], df['mentions'], df['links'], df['text'], df['words'] = zip(*df['text'].apply(get_textdict))
+    del df['text']
     
     # get tweet location
-    [df['longtitude'], df['latitude']] = df['place/bounding_box/coordinates'].apply(get_location)
+    df['longitude'], df['latitude'] = zip(*df['place/bounding_box/coordinates'].apply(get_location))
+    del df['place/bounding_box/coordinates']
     
-    # alleen nog:
-    
-    """
-    - Adjusted time
-    
-    """
-    
-    # fill dataframe    
-    #df['longitude'] = longitude
-    #df['latitude'] = latitude
-    df['date'] = (df['timestamp_ms'].apply(int)/ 1e3).apply(datetime.datetime.fromtimestamp)
-    df['new-york time'] = 0.3
+    # get adjusted time and date
+    df['datetime'] = (df['timestamp_ms'].apply(int)/ 1e3).apply(datetime.datetime.fromtimestamp)
+    del df['timestamp_ms']
+
     return df
 
 def tweet_to_dict(tweet,relevant_columns):
@@ -135,20 +111,3 @@ if __name__ == "__main__":
     # Preprocess the data into our ideal dataframe
     df = preprocess_dataframe(df)
     pp.pprint(df)
-    
-    
-    # add column with sentiment of each tweet
-    #df_with_sentiment = add_sentiment(df)
-    
-    
-    # overbodige comments
-    # coordinates
-    # created_at
-    # place / boundingbox / coordinates (midden)
-    # geo
-    # text (mentions, text, plaatjes link)
-    # time zone
-    
-    
-    
-    
