41fa6eee2416036a2caf0c3130600a06cac0ed7d
stefanhaannl <31733054+stefanhaannl@users.noreply.github.com>
2017-09-21 12:36:30 +0200

    changed error
    Changed error and added fuction

--- Week2-3/topic-analysis.py
+++ Week2-3/topic-analysis.py
@@ -8,4 +8,12 @@ def load_tweets(filepath):
     OUTPUT: Pandas series of tweet documents (as lists)
     """
     df = pd.read_pickle(filepath)
-    return df[['text','hashtags']]
+    return df[['words','hashtags']]
+
+def cluster_by_hashtags(df,n):
+    """
+    Generates a corpus based on the n'th most prevalent hashtags
+    INPUT: Dataframe with hashtags (list) and text (list) column
+    OUTPUT: Dataframe with hashtag (str) and text(list) column
+    """
+    pass
