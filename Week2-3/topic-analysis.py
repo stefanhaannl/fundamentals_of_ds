3e1ec30c58f7ce0d3aebd61898ac4b6ca37ed363
DeWvanAxel <31886145+DeWvanAxel@users.noreply.github.com>
2017-09-21 12:29:23 +0200

    Fix that lemma and stopwords are defined

--- Week2-3/topic-analysis.py
+++ Week2-3/topic-analysis.py
@@ -6,9 +6,29 @@ from nltk.stem.wordnet import WordNetLemmatizer
 import string
 
 def clean(doc):
+	"""
+	Remove stopwords from string and normalize words in string
+	INPUT: List of strings(which are the documents)
+	OUTPUT: List of list of words
+	"""
     stop_free = " ".join([i for i in doc.lower().split() if i not in stop])
     normalized = " ".join(lemma.lemmatize(word) for word in stop_free.split())
     return normalized
+
+def load_tweets(filepath):
+    """
+    Load an array of tweets two work with. (pickle file)
+    INPUT: filepath to your pickle file
+    OUTPUT: Pandas series of tweet documents (as lists)
+    """
+    df = pd.read_pickle(filepath)
+    return df[['text','hashtags']]
+
+# Create a set of stopwords
+stop = set(stopwords.words('english'))
+
+# This is the function makeing the lemmatization
+lemma = WordNetLemmatizer()
 
 doc1 = "Working out is great for the body. Fitness makes you feel good."
 doc2 = "Red cars are faster than blue cars."
@@ -19,14 +39,5 @@ doc5 = "Pushups make a good upper body excercise."
 # compile documents
 doc_complete = [doc1, doc2, doc3, doc4, doc5]
 doc_clean = [clean(doc).split() for doc in doc_complete] 
-=======
 
-def load_tweets(filepath):
-    """
-    Load an array of tweets two work with. (pickle file)
-    INPUT: filepath to your pickle file
-    OUTPUT: Pandas series of tweet documents (as lists)
-    """
-    df = pd.read_pickle(filepath)
-    return df[['text','hashtags']]
->>>>>>> 4c70b451cf31cb574b0e782bde3159d04cb34a67
+
