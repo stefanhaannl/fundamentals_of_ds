4c70b451cf31cb574b0e782bde3159d04cb34a67
stefanhaannl <31733054+stefanhaannl@users.noreply.github.com>
2017-09-21 12:22:31 +0200

    load data function
    Added a function which enables to load data from pickle

--- Week2-3/topic-analysis.py
+++ Week2-3/topic-analysis.py
@@ -1,2 +1,11 @@
 import numpy as np
 import pandas as pd
+
+def load_tweets(filepath):
+    """
+    Load an array of tweets two work with. (pickle file)
+    INPUT: filepath to your pickle file
+    OUTPUT: Pandas series of tweet documents (as lists)
+    """
+    df = pd.read_pickle(filepath)
+    return df[['text','hashtags']]
