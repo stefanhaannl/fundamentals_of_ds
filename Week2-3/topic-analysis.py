d046830465ae5e52402e34090e13c5d4bff34d29
stefanhaannl <31733054+stefanhaannl@users.noreply.github.com>
2017-09-21 13:22:45 +0200

    cluster_by_hashtags
    Custom function to cluster by hashtags written an tested.

--- Week2-3/topic-analysis.py
+++ Week2-3/topic-analysis.py
@@ -1,16 +1,15 @@
 import numpy as np
 import pandas as pd
-<<<<<<< HEAD
 from nltk.corpus import stopwords 
 from nltk.stem.wordnet import WordNetLemmatizer
 import string
 
 def clean(doc):
-	"""
-	Remove stopwords from string and normalize words in string
-	INPUT: List of strings(which are the documents)
-	OUTPUT: List of list of words
-	"""
+    """
+    Remove stopwords from string and normalize words in string
+    INPUT: List of strings(which are the documents)
+    OUTPUT: List of list of words
+    """
     stop_free = " ".join([i for i in doc.lower().split() if i not in stop])
     normalized = " ".join(lemma.lemmatize(word) for word in stop_free.split())
     return normalized
@@ -30,21 +29,23 @@ def cluster_by_hashtags(df,n):
     INPUT: Dataframe with hashtags (list) and text (list) column
     OUTPUT: Dataframe with hashtag (str) and text(list) column
     """
-    pass
+    result_df = []
+    hashtags = []
+    for lst in df['hashtags']:
+        hashtags.extend(lst)
+    hashtags = pd.Series(hashtags)
+    hashtag_values = hashtags.value_counts()[0:n]
+    hashtags_list = hashtag_values.index.values.tolist()
+    for hashtag in hashtags_list:
+        print "Filtering words for hashtag: "+hashtag
+        words = []
+        for row in df.itertuples():
+            row = tuple(row)
+            if hashtag in row[2]:
+                words.extend(row[1])
+        result_df.append([hashtag, words])
+    return pd.DataFrame(result_df,columns=['hashtag','words'])
+    
 
-# Create a set of stopwords
-stop = set(stopwords.words('english'))
 
-# This is the function makeing the lemmatization
-lemma = WordNetLemmatizer()
-
-doc1 = "Working out is great for the body. Fitness makes you feel good."
-doc2 = "Red cars are faster than blue cars."
-doc3 = "Doctors suggest that fitness increases muscle mass and speeds up metabolism."
-doc4 = "Cars with electrical engines cause less polution than cars with internal combustion engines."
-doc5 = "Pushups make a good upper body excercise."
-
-# compile documents
-doc_complete = [doc1, doc2, doc3, doc4, doc5]
-doc_clean = [clean(doc).split() for doc in doc_complete] 
 
