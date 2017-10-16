97efd98476bb94268a6ce9a45334c97dac498d31
Daniel <danielfun@live.nl>
2017-10-16 13:32:40 +0200

    nog een keer

--- Week6-7/prepocessing.py
+++ Week6-7/prepocessing.py
@@ -25,44 +25,48 @@ def anp(path, init_df):
 # In[face]
 
 def face(path, init_df):
-
+    
+    return face_df
     
 # In[image_data]
 
 def image_data(path, init_df):
-
+    
+    return image_data_df
     
 # In[image_metrics]
 
 def image_metrics(path, init_df): 
     
+    return image_metrics_df
     
 # In[survey]
 
 def survey(path, init_df):
     
+    return survey_df
     
 # In[object_labels]
 
 def object_labels(path, init_df):
 
+    return object_labels_df
     
 # In[main]
 if __name__ == "__main__": 
     
     paths = ['data/anp.pkl','data/face.pkl','data/image_data.pkl',
-         'data/image_metrics.pkl','data/object_labels.pkl',
-         'data/survey.pkl','data/object_labels.pkl']
+         'data/image_metrics.pkl','data/survey.pkl','data/object_labels.pkl']
 
     init_df = init(paths[2])
     
     # Load al the data
-    anp_df = anp(paths[0])
-    face_df = anp(paths[1])
-    image_data_df = anp(paths[2])
-    image_metrics_df = anp(paths[3])
-    survey_df = anp(paths[4])
-    object_labels_df = anp(paths[5])
+    anp_df = anp(paths[0], init_df)
+    face_df = face(paths[1], init_df)
+    image_data_df = image_data(paths[2], init_df)
+    image_metrics_df = image_metrics(paths[3], init_df)
+    survey_df = survey(paths[4], init_df)
+    object_labels_df = object_labels(paths[5], init_df)
     
     
 
